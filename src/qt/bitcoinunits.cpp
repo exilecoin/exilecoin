c7c934b58d82b5d352564fa8ef006036e3d51090
exile <nicholasdbarron@gmail.com>
2014-04-16 04:05:04 -0700

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("EXC");
+    case mBTC: return QString("mEXC");
+    case uBTC: return QString::fromUtf8("μEXC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("ExileCoin");
+    case mBTC: return QString("Milli-ExileCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-ExileCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
